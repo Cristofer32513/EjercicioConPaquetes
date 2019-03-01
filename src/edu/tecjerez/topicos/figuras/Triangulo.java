05d9fe3fbf14980e78096ae7ffb69c11b24b6aee
Cristofer Casas <cristofer32513@gmail.com>
2019-03-01 16:16:09 -0600

    Se agrego la clase Triangulo con el metodo para calcular el area mediante la formula de Heron al paquete de figuras.

---
+++ src/edu/tecjerez/topicos/figuras/Triangulo.java
@@ -0,0 +1,14 @@
+package edu.tecjerez.topicos.figuras;
+
+public class Triangulo {
+	//Metodo que calcule el area de un triangulo a atravez de la formula de Heron
+	
+	
+	public double area(double a, double b, double c){
+		double s=(a+b+c)/2;
+		
+		double area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+		
+		return area;
+	}
+}
